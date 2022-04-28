0909557ec59ffeec6b291a8d39ed7ee765c63df3
Pliv <plivbotgod@gmail.com>
2022-04-28 13:51:37 +0200

    added both in 1 line so i can execute 1 thing

---
+++ Pliv.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/ESP/main/ESP.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/ESP/main/HitBoxExstender.lua"))()
