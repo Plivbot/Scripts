4a92e42bd412d0d16ae908e8c52f71d2eaaa06cf
Pliv <plivbotgod@gmail.com>
2022-05-16 22:02:34 +0200

    Update Pliv.lua

--- Pliv.lua
+++ Pliv.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/ESP/main/ESP.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/ESP/main/box.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/Scripts/main/ESP.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/Scripts/main/box.lua"))()
