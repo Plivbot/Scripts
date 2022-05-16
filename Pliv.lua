47357b86d252d1fd3edaf41aeed60b6be5dd9cbc
Pliv <plivbotgod@gmail.com>
2022-05-16 21:49:39 +0200

    Update Pliv.lua

--- Pliv.lua
+++ Pliv.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/ESP/main/ESP.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/ESP/main/HitBoxExstender.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/ESP/main/HitBoxExstender2.lua"))()
