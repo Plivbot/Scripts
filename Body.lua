252f74bf52ee8bc0d8b1c844eff6a73ef10dc6fd
Pliv <plivbotgod@gmail.com>
2022-09-05 02:16:30 +0200

    Add files via upload

---
+++ Body.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/Scripts/main/EspBodyHit.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/Scripts/main/bodyHitbox.lua"))()
