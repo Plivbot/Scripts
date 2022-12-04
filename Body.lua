fa1c74d01a1874a12854f05ec9774303f702fda7
Pliv <plivbotgod@gmail.com>
2022-12-04 12:29:08 +0100

    moved things

--- Body.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/Scripts/main/EspBodyHit.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/Scripts/main/bodyHitbox.lua"))()
---
+++ LoadStrings/Test.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/Scripts/main/highlight.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Plivbot/Scripts/main/bodyHitbox.lua"))()
